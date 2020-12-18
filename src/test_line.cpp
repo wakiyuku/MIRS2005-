9a09724a60e0f73210548183474268918f4accfe
wakiyuku <d17141@numazu.kosen-ac.jp>
2020-12-18 16:55:59 +0900

    標準出力

--- src/test_line.cpp
+++ src/test_line.cpp
@@ -3,15 +3,16 @@
 #include <vector>
 #include "arduino.h"
 #include "request.h"
-#define PGAIN 0.5f
-#define DGAIN 0.01f
-#define IGAIN 0.00f
+#define PGAIN 1f
+#define DGAIN 0.0002f
+#define IGAIN 0.0005f
 #define B_MAX 100
 #define B_MIN 0
 #define G_MAX 100
 #define G_MIN 0
 #define R_MAX 255
 #define R_MIN 100
+#define RASIO 0.5f
 using namespace std;
 using namespace cv;
 
@@ -22,9 +23,9 @@ int Capture(Mat*);
 int CheckColor(Mat rbg, int width, int height);
 int CheckColor(Mat rbg, int width, int height);
 float Rasio(float , float);
-void GetPGAIN(int* plspeed, int* prspeed,int lwhitepix ,int rwhitepix,int speed,int base_speed);
-void GetDGAIN(int* dlspeed, int* drspeed,int* past_l_pix, int* past_r_pix, int lwhitepix, int rwhitepix);
-void GetIGAIN(int* ILspeed,int* IRspeed,int*accum,int lwhitepix,int rwhitepix);
+void GetPGAIN(float* plspeed, float* prspeed,int lwhitepix ,int rwhitepix,int speed,int base_speed);
+void GetDGAIN(float* dlspeed, float* drspeed,int* past_l_pix, int* past_r_pix, int lwhitepix, int rwhitepix);
+void GetIGAIN(float* ILspeed,float* IRspeed,float*accum,int* past_l_pix, int* past_r_pix,int lwhitepix,int rwhitepix);
 
 VideoCapture cap(0);//デバイスのオープン
 
@@ -35,20 +36,22 @@ int main()
 	//変数宣言
 	Vec4f output;
 	Mat rbg, gray,bin;
-	int width, height;
+	int width, height,rwhitepix,lwhitepix;
 	//P制御用変数
-	int speed, base_speed,PRspeed,PLspeed;
+	float PRspeed,PLspeed;
+	int speed, base_speed;
 	speed = 100;
 	base_speed = 50;
 	PRspeed = PLspeed = 0;
 	//D制御用変数
-	int past_r_pix,past_l_pix,DRspeed, DLspeed;
+	float DRspeed, DLspeed;
+	int past_r_pix,past_l_pix;
 	past_l_pix=past_r_pix=DRspeed=DLspeed=0;
 	//I制御用変数
-	int accum,IRspeed,ILspeed;
+	float accum,IRspeed,ILspeed;
 	accum = IRspeed = ILspeed = 0;
 	//制御後速度
-	int l_speed, r_speed;
+	float l_speed, r_speed;
 	l_speed = 0;
 	r_speed = 0;
 
@@ -63,23 +66,29 @@ int main()
 		//左右のピクセル数を比べて出力
 		printf("count\n");
 		//左右白色ピクセル数を取得
-		int rwhitepix = RightTrim(bin, height, width);
-		int lwhitepix = LeftTrim(bin, height, width);
-		printf("%d\n",rwhitepix);
-		printf("%d\n",lwhitepix);
+		rwhitepix = RightTrim(bin, height, width);
+		lwhitepix = LeftTrim(bin, height, width);
+		printf("rightpix=%d\n",rwhitepix);
+		printf("leftpix =%d\n",lwhitepix);
 		//P制御値設定
 		GetPGAIN(&PLspeed, &PRspeed, lwhitepix, rwhitepix,speed,base_speed);
+		printf("PRspeed=%f\n",PRspeed);
+		printf("PLspeed=%f\n",PLspeed);
 		//D制御値設定
 		GetDGAIN(&DLspeed, &DRspeed, &past_l_pix, &past_r_pix, lwhitepix, rwhitepix);
+		printf("DRspeed=%f\n",DRspeed);
+		printf("DLspeed=%f\n",DLspeed);
 		//I制御値設定
-		GetIGAIN(&ILspeed,&IRspeed,&accum,lwhitepix, rwhitepix);
+		GetIGAIN(&ILspeed,&IRspeed,&accum,&past_l_pix, &past_r_pix, lwhitepix,rwhitepix);
+		printf("IRspeed=%f\n",IRspeed);
+		printf("ILspeed=%f\n",ILspeed);
 		//走行スピード設定
-		l_speed = PLspeed+DGAIN*DLspeed+IGAIN*ILspeed;
-		r_speed = PRspeed+DGAIN*DRspeed+IGAIN*IRspeed;
-		printf("PL\n%d\n",DLspeed);
-		printf("PR\n%d\n\n\n",DRspeed);
+		l_speed = (PLspeed+DGAIN*DLspeed+IGAIN*ILspeed)*RASIO;
+		r_speed = (PRspeed+DGAIN*DRspeed+IGAIN*IRspeed)*RASIO;
+		printf("r_speed%f\n",r_speed);
+		printf("l_speed%f\n\n\n",l_speed);
 		//走行スピード設定
-		printf("redpix=%d\n",CheckColor(rbg, width, height));
+		//printf("redpix=%d\n",CheckColor(rbg, width, height));
 		if(CheckColor(rbg, width, height)>3000){
 			request_set_runmode(STP, 0, 0);
 			printf("STOP!!!!\n");
@@ -142,7 +151,7 @@ int CheckColor(Mat rbg, int width, int height) {
 }
 
 //Pゲイン設定
-void GetPGAIN(int* plspeed, int* prspeed,int lwhitepix ,int rwhitepix,int speed,int base_speed){
+void GetPGAIN(float* plspeed, float* prspeed,int lwhitepix ,int rwhitepix,int speed,int base_speed){
 	float pixRasio = Rasio(rwhitepix, lwhitepix);
 	printf("rasio=");
 	printf("%f\n",pixRasio);
@@ -163,16 +172,16 @@ float Rasio(float r, float l){
 	}
 }
 //Dゲイン設定
-void GetDGAIN(int* dlspeed, int* drspeed,int* past_l_pix, int* past_r_pix, int lwhitepix, int rwhitepix){
-	printf("%d,%d,%d,%d\n",*past_l_pix,*past_r_pix,lwhitepix,lwhitepix);
-		*dlspeed = lwhitepix-*past_l_pix;
-		*drspeed = rwhitepix-*past_r_pix;
-		*past_l_pix=lwhitepix;
-		*past_r_pix=rwhitepix;
+void GetDGAIN(float* dlspeed, float* drspeed,int* past_l_pix, int* past_r_pix, int lwhitepix, int rwhitepix){
+	//printf("%d,%d,%d,%d\n",*past_l_pix,*past_r_pix,lwhitepix,lwhitepix);
+	*dlspeed = lwhitepix-*past_l_pix;
+	*drspeed = rwhitepix-*past_r_pix;
+	*past_l_pix=lwhitepix;
+	*past_r_pix=rwhitepix;
 }
 //Iゲイン設定
-void GetIGAIN(int* ILspeed,int* IRspeed,int*accum,int lwhitepix,int rwhitepix){
-	*accum+=(lwhitepix-rwhitepix)/1000;
+void GetIGAIN(float* ILspeed,float* IRspeed,float*accum,int* past_l_pix, int* past_r_pix,int lwhitepix,int rwhitepix){
+	*accum=(*past_l_pix+lwhitepix-*past_r_pix+rwhitepix)/2;
 	if(accum==0){
 		*ILspeed = 0;
 		*IRspeed = 0;
@@ -180,7 +189,7 @@ void GetIGAIN(int* ILspeed,int* IRspeed,int*accum,int lwhitepix,int rwhitepix){
 		*ILspeed = -*accum;
 		*IRspeed = *accum;
 	}
-	printf("%d,%d,%d\n",*accum,*ILspeed,*IRspeed);
+	//printf("%d,%d,%d\n",*accum,*ILspeed,*IRspeed);
 }
 
 
